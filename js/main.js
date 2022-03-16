e89d3492bca1d0ef79ef21d0975c8c0e20ff047b
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-03-16 07:12:25 +0600

    works and skills section added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+    // Mixitup Effect Portfolio
+
+    let mixerWork = mixitup('.box_container', {
+        selectors: {
+            target: '.box_single'
+        },
+        animation: {
+            duration: 500
+        }
+    });
+
+
 const burger = document.querySelector(".hamburger");
 const menu = document.querySelector(".nav-menu")
 
@@ -10,3 +22,72 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     burger.classList.remove("active")
     menu.classList.remove("active")
 }))
+
+// Work Portfolio Countdown
+
+let number = document.getElementById("number");
+let numberTwo = document.getElementById("numberTwo");
+let numberThree = document.getElementById("numberThree");
+let numberFour = document.getElementById("numberfour");
+let numberFive = document.getElementById("numberfive");
+let numberSix = document.getElementById("numbersix");
+let counter = 0;
+let counterTwo = 0;
+let counterThree = 0;
+let counterFour = 0;
+let counterFive = 0;
+let counterSix = 0;
+
+setInterval(() => {
+    if(counter == 85){
+        clearInterval();
+    }else{
+        counter += 1;
+        number.innerHTML = counter + "%"
+    }
+}, 19);
+
+setInterval(() => {
+    if(counterTwo == 80){
+        clearInterval();
+    }else{
+        counterTwo += 1;
+        numberTwo.innerHTML = counter + "%"
+    }
+}, 19);
+
+setInterval(() => {
+    if(counterThree == 50){
+        clearInterval();
+    }else{
+        counterThree += 1;
+        numberThree.innerHTML = counter + "%"
+    }
+}, 19);
+
+setInterval(() => {
+    if(counterFour == 80){
+        clearInterval();
+    }else{
+        counterFour += 1;
+        numberFour.innerHTML = counter + "%"
+    }
+}, 19);
+
+setInterval(() => {
+    if(counterFive == 40){
+        clearInterval();
+    }else{
+        counterFive += 1;
+        numberFive.innerHTML = counter + "%"
+    }
+}, 19);
+
+setInterval(() => {
+    if(counterSix == 10){
+        clearInterval();
+    }else{
+        counterSix += 1;
+        numberSix.innerHTML = counter + "%"
+    }
+}, 19);
