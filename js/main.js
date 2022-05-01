4f427ee90eef153b812c37fa09a06dc2145b44ff
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-05-02 02:14:39 +0600

    timer function has been cancelled

--- js/main.js
+++ js/main.js
@@ -21,74 +21,72 @@ document.querySelectorAll(".nav-link").forEach((n) =>
 );
 
 // Work Portfolio Countdown
-window.addEventListener("scroll", () => {
-  let number = document.getElementById("number");
-  let numberTwo = document.getElementById("numberTwo");
-  let numberThree = document.getElementById("numberThree");
-  let numberFour = document.getElementById("numberfour");
-  let numberFive = document.getElementById("numberfive");
-  let numberSix = document.getElementById("numbersix");
-  let counter = 0;
-  let counterTwo = 0;
-  let counterThree = 0;
-  let counterFour = 0;
-  let counterFive = 0;
-  let counterSix = 0;
+let number = document.getElementById("number");
+let numberTwo = document.getElementById("numberTwo");
+let numberThree = document.getElementById("numberThree");
+let numberFour = document.getElementById("numberfour");
+let numberFive = document.getElementById("numberfive");
+let numberSix = document.getElementById("numbersix");
+let counter = 0;
+let counterTwo = 0;
+let counterThree = 0;
+let counterFour = 0;
+let counterFive = 0;
+let counterSix = 0;
 
-  setInterval(() => {
-    if (counter == 85) {
-      clearInterval();
-    } else {
-      counter += 1;
-      number.innerHTML = counter + "%";
-    }
-  }, 19);
+setInterval(() => {
+  if (counter == 85) {
+    clearInterval();
+  } else {
+    counter += 1;
+    number.innerHTML = counter + "%";
+  }
+}, 19);
 
-  setInterval(() => {
-    if (counterTwo == 80) {
-      clearInterval();
-    } else {
-      counterTwo += 1;
-      numberTwo.innerHTML = counter + "%";
-    }
-  }, 19);
+setInterval(() => {
+  if (counterTwo == 80) {
+    clearInterval();
+  } else {
+    counterTwo += 1;
+    numberTwo.innerHTML = counter + "%";
+  }
+}, 19);
 
-  setInterval(() => {
-    if (counterThree == 50) {
-      clearInterval();
-    } else {
-      counterThree += 1;
-      numberThree.innerHTML = counter + "%";
-    }
-  }, 19);
+setInterval(() => {
+  if (counterThree == 50) {
+    clearInterval();
+  } else {
+    counterThree += 1;
+    numberThree.innerHTML = counter + "%";
+  }
+}, 19);
 
-  setInterval(() => {
-    if (counterFour == 80) {
-      clearInterval();
-    } else {
-      counterFour += 1;
-      numberFour.innerHTML = counter + "%";
-    }
-  }, 19);
+setInterval(() => {
+  if (counterFour == 80) {
+    clearInterval();
+  } else {
+    counterFour += 1;
+    numberFour.innerHTML = counter + "%";
+  }
+}, 19);
 
-  setInterval(() => {
-    if (counterFive == 40) {
-      clearInterval();
-    } else {
-      counterFive += 1;
-      numberFive.innerHTML = counter + "%";
-    }
-  }, 19);
+setInterval(() => {
+  if (counterFive == 40) {
+    clearInterval();
+  } else {
+    counterFive += 1;
+    numberFive.innerHTML = counter + "%";
+  }
+}, 19);
 
-  setInterval(() => {
-    if (counterSix == 50) {
-      clearInterval();
-    } else {
-      counterSix += 1;
-      numberSix.innerHTML = counter + "%";
-    }
-  }, 19);
-});
+setInterval(() => {
+  if (counterSix == 50) {
+    clearInterval();
+  } else {
+    counterSix += 1;
+    numberSix.innerHTML = counter + "%";
+  }
+}, 19);
 
 // Swipper Navigation Code
 
